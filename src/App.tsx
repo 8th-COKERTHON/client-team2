600123888a8d87f9dd2f6c807c37f55a396dd2e7
metjyc <metjyc@naver.com>
2026-07-11 03:22:16 +0900

    fix: type auth route exclusions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BottomNavigation } from "@/components/layout/BottomNavigation";
 import { ROUTES } from "@/constants/routes";
 import { useLocation } from "react-router";
 
-const ROUTES_WITHOUT_BOTTOM_NAV = [
+const ROUTES_WITHOUT_BOTTOM_NAV: string[] = [
   ROUTES.splash,
   ROUTES.authEntry,
   ROUTES.login,
